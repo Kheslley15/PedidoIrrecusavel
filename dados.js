9274ed08f0a0caa4784436717c4c3fd0de6ef178
Kheslley Rodrigues <139723181+Kheslley15@users.noreply.github.com>
2023-10-06 00:34:04 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,11 @@
+function enviar(){
+ 
+    var nome = document.getElementById("txtnome");
+    var telefone = document.getElementById("txttelefone");
+    var email = document.getElementById("txtemail");
+     
+    var texto = nome + telefone + email; // como quiser formatar
+     
+    return true;
+     
+    }
